ee0f721ec107c72e44cef560eae2a26e43c89f36
theeasylearn <theeasylearn@gmail.com>
2025-12-31 19:24:17 +0530

    test

---
+++ 4_tuple_method.py
@@ -0,0 +1,8 @@
+#create tuple
+gods = ('Bramha','Vishnu','Mahesh','Bramha')
+print(gods)
+print("Count no of Bramha ",gods.count('Bramha'))
+print("position of Vishnu ",gods.index('Vishnu'))
+# print("position of Hanuman ",gods.index('Hanuman'))
+# print("position of Krishna ",gods.index('Krishna'))
+print("Good bye.")
