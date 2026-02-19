456aaa96029fd4294296ab7d28869937c5a984cf
theeasylearn <theeasylearn@gmail.com>
2026-02-19 20:32:04 +0530

    oops

---
+++ 42_oop_1.py
@@ -0,0 +1,21 @@
+#create class
+class Human:
+    def walk(self):
+        print("I can walk")
+    def talk(self):
+        print("I can talk")
+    def sleep(self):
+        print("I can sleep")
+
+#create object
+ronit = Human() 
+ronit.walk()
+ronit.talk()
+ronit.sleep()
+
+#create another object
+ankit = Human()
+ankit.sleep()
+ankit.walk()
+ankit.talk()
+
