26cd826d091c76b840dabd68d63db288589aa63d
theeasylearn <theeasylearn@gmail.com>
2026-04-11 20:32:26 +0530

    update demo

---
+++ project/1_project.py
@@ -0,0 +1,82 @@
+import connect as cn 
+from datetime import datetime as dt 
+mycursor = cn.database.cursor(dictionary=True)
+while True:    
+    print("Press 1 to display trip")
+    print("Press 2 to add new trip")
+    print("Press 3 to update existing trip")
+    print("Press 4 to delete existing trip")
+    print("Press 0 to exit")
+    trip_choice = int(input("Enter trip choice"))
+    if trip_choice<0 or trip_choice>4:
+        print("Invalid trip choice")
+    elif trip_choice==1:
+        #create sql statement
+        sql = "select id,title,detail,date_format(start_date,'%d-%m-%Y') as start_date ,days from trip order by id desc";
+
+        #run sql statement
+        mycursor.execute(sql) 
+
+        #fetch one row 
+        # firstrow = mycursor.fetchone()
+        # print(firstrow)
+
+        #fetch all rows 
+        table = mycursor.fetchall()
+
+        #display 
+        # print(table)
+        print(f"{'id':<10} {'title':<32} {'detail':<32} {'start date'} {'':<18} {'days':<10}")
+        print("_"*112)
+        for row in table:
+            print(f"{row['id']:<10} {row['title']:<32} {row['detail']:<32} {row['start_date']} {'':<12} {row['days']:10}")
+    elif trip_choice==2:
+        sql = "insert into trip (title,detail,start_date,days) values (%s,%s,%s,%s)"
+        #accept input from user 
+        title = input("Enter trip title")
+        detail = input("Enter trip detail")
+        start_date = input("Enter Trip date (DD-MM-YYYY)")
+        #convert string date into date 
+        try:
+            start_date = dt.strptime(start_date,"%d-%m-%Y")
+            days = int(input("Enter trip days"))
+            #create list that has 4 values 
+            # values = ['Dwarka Trip','Trip to Dwarka and somnath','2026-04-15',5]
+            values = [title,detail,start_date,days]
+            #run sql statement 
+            mycursor.execute(sql,values)
+            cn.database.commit() #required to save changes into database 
+            print(mycursor.rowcount," Trip Inserted")
+        except ValueError as e:
+            print("invalid date")
+    elif trip_choice==3:
+        try:
+            #create sql statement 
+            sql = "update trip set title=%s,detail=%s,start_date=%s,days=%s where id=%s"
+            #create variable & accept input
+            title = input("Enter new trip title")
+            detail = input("Enter new trip detail")
+            start_date = input("Enter new start date (DD-MM-YYYY)")
+            start_date = dt.strptime(start_date,"%d-%m-%Y")
+            days = int(input("Enter revised trip days"))
+            id = int(input("Enter trip id to update"))
+            #create list 
+            values = [title,detail,start_date,days,id]
+            #execute sql statement
+            mycursor.execute(sql,values)
+            cn.database.commit()
+        except ValueError as Error:
+            print("invalid date")
+        else:
+            print(mycursor.rowcount, " trip updated")
+    elif trip_choice==4:
+        sql = "delete from trip where id=%s"
+        id = int(input("Enter trip id to delete"))
+        values = [id]
+        mycursor.execute(sql,values)
+        cn.database.commit()
+        print(mycursor.rowcount, " trip deleted")
+    else:
+        print("good bye.") 
+        break # loop break(stop)
+    key = input("Press enter to continue")    
--- project/update_demo.py
+++ project/update_demo.py
@@ -1,1 +1,30 @@
 import connect as cn 
+from datetime import datetime as dt 
+try:
+    #create cursor object
+    mycursor = cn.database.cursor()
+
+    #create sql statement 
+    sql = "update trip set title=%s,detail=%s,start_date=%s,days=%s where id=%s"
+
+    #create variable & accept input
+    title = input("Enter new trip title")
+    detail = input("Enter new trip detail")
+    start_date = input("Enter new start date (DD-MM-YYYY)")
+    start_date = dt.strptime(start_date,"%d-%m-%Y")
+    days = int(input("Enter revised trip days"))
+    id = int(input("Enter trip id to update"))
+
+    #create list 
+    values = [title,detail,start_date,days,id]
+
+    #execute sql statement
+    mycursor.execute(sql,values)
+
+    cn.database.commit()
+except ValueError as Error:
+    print("invalid date")
+else:
+    print(mycursor.rowcount, " trip updated")
+finally:
+    print("good bye.")
